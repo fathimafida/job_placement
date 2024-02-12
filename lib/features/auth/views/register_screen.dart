a581714d5574bec41d298bde3f519af48a38a33b
Fathima Fida <fidafirozkattungal@gmail.com>
2024-02-12 18:30:00 +0530

    register page sdded

--- lib/features/auth/views/register_screen.dart
+++ lib/features/auth/views/register_screen.dart
@@ -1,1 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:job_placement/common/utils/utils.dart';
+import 'package:job_placement/features/auth/views/auth_screen.dart';
 
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _userController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _userController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter username";
+                  }
+                  return null;
+                },
+                onTapOutside: (event) {
+                  FocusScope.of(context).unfocus();
+                },
+                decoration: InputDecoration(
+                  hintText: "username",
+                  label: Text("Username"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: _emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a valid email";
+                  } else if (!value.contains('@') || !value.contains('.')) {
+                    return "Please enter a valid email";
+                  }
+                  return null;
+                },
+                onTapOutside: (event) {
+                  FocusScope.of(context).unfocus();
+                },
+                decoration: InputDecoration(
+                  hintText: "email",
+                  label: Text("Email"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a valid password";
+                  } else if (value.length < 3) {
+                    return "Please enter a valid email";
+                  }
+                  return null;
+                },
+                onTapOutside: (event) {
+                  FocusScope.of(context).unfocus();
+                },
+                decoration: InputDecoration(
+                  hintText: "password",
+                  label: Text("Password"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Row(children: [
+                Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot Password?",
+                    style: applyTextStyle(16, FontWeight.bold),
+                  ),
+                ),
+              ]),
+              SizedBox(
+                height: (30),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    navigateTO(context, AuthScreen());
+                  }
+                },
+                child: Text(
+                  "Register",
+                  style: applyTextStyle(20, FontWeight.bold),
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Theme.of(context).colorScheme.primaryContainer,
+                    foregroundColor:
+                        Theme.of(context).colorScheme.tertiaryContainer,
+                    minimumSize: Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
